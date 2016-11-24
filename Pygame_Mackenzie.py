cc878f24a131832a84fa473e1563436093a19869
Dr Brown <Dr Brown>
2016-11-24 18:29:59 -0500

    sixth commit

--- Pygame_Mackenzie.py
+++ Pygame_Mackenzie.py
@@ -6,7 +6,9 @@ import random
 pygame.init();
 
 WIDTH = 800
-HEIGHT = 800
+HEIGHT = 600
+
+screen_height = 800 
 
 white = (255,255,255)
 black = (0,0,0)
@@ -27,11 +29,15 @@ x_delta = 0
 y_delta = 0
 clock = pygame.time.Clock()
 
+meatballspeed = 10
+maxmeatballs = 5
+spaghettispeed = 10
+maxspaghettis = 5
 #create a surface
-gameDisplay = pygame.display.set_mode((800,600)) #initialize with a tuple
+gameDisplay = display.set_mode((WIDTH, HEIGHT)) #initialize with a tuple
 
 #lets add a title, aka "caption"
-pygame.display.set_caption("Cloudy with a Chance of Meatballs")
+display.set_caption("Cloudy with a Chance of Meatballs")
 
 #pygame.display.flip() 		#similar to a flip book, updates entire surface
 pygame.display.update()		#only updates portion specified
@@ -42,33 +48,83 @@ class Meatball(Sprite):
 		self.image = image.load("meatball.bmp").convert()
 		self.rect = self.image.get_rect()
 
-meatballs1 = image.load("meatball.bmp").convert()
-
-	# def move(self):
-	# 	randX= randint(0, 300)
-	# 	randY = randint(450, 0)
-	# 	self.rect.center = (randX, randY)
+	def update(self):
+		self.rect.y += meatballspeed
+		if self.rect.y > screen_height:
+			self.rect.x = random.randint(1,WIDTH -20)
+			self.rect.y = 100
+			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
 
 class Spaghetti(Sprite):
 	def __init__(self):
 		Sprite.__init__(self)
-		self.image = image.load("Spaghetti.jpg").convert()
-		self.rect= self.image.get_rect()
+		self.image = image.load("spaghetti.bmp").convert()
+		self.rect = self.image.get_rect()
 
-	def move(self):
-		randiX= randint(600, 500)
-		randiY = randint(0,500)
-		self.rect.center = (randiX, randiY)
+	def update(self):
+		self.rect.y += spaghettispeed
+		if self.rect.y > screen_height:
+			self.rect.x = random.randint(1,WIDTH -20)
+			self.rect.y = 100
+			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
 
-pygame.display.update()		#only updates portion specified
+meatballArray = []
+for i in range(0, maxmeatballs-1):
+	meatballArray.append(Meatball())
+meatballSprites = []
+for x in  range(0, (len(meatballArray)-1)):
+	meatballSprites.append(RenderPlain(meatballArray[x]))
+
+spaghettiArray = []
+for i in range(0, maxspaghettis-1):
+	spaghettiArray.append(Spaghetti())
+spaghettiSprites = []
+for x in  range(0, (len(spaghettiArray)-1)):
+	spaghettiSprites.append(RenderPlain(spaghettiArray[x]))
+
+class Bowl(Sprite):
+    def __init__(self):
+    	Sprite.__init__(self)
+    	self.image = image.load("bowl.bmp").convert()
+		self.rect = self.image.get_rect()
+
+	def move(ammount):
+		self.x += ammount
+		self.y 
+
+        
+# block = Meatball([20, 20])
+
+# meatballs1 = image.load("meatball.bmp").convert()
+	# def move(self):
+	# 	randX = randint(0, 600)
+
+	# 	randY = randint(0, 400)
+	# 	self.rect.center = (randX,randY)
+
+# class Spaghetti(Sprite):
+# 	def __init__(self):
+# 		Sprite.__init__(self)
+# 		self.image = image.load("Spaghetti.bmp").convert()
+# 		self.rect= self.image.get_rect()
+
+# 	def move(self):
+# 		randiX= randint(600, 500)
+# 		randiY = randint(0,500)
+# 		self.rect.center = (randiX, randiY)
+
+# pygame.display.update()		#only updates portion specified
 		
 
 init()
 
-meatballs = Meatball()
-sprites = RenderPlain(meatballs)
-# def hit(self,target):
-# 	return self.rect.colliderect(target)
+
+
+# spaghetti1 = Spaghetti()
+# meatballs1 = Meatball([20,20])
+# sprites = RenderPlain(meatballs1)
+def hit(self,target):
+	return self.rect.colliderect(target)
 #this is when it collides
 
 gameExit = False
@@ -79,6 +135,7 @@ while not gameExit:
 		if event.type == pygame.QUIT:
 			gameExit = True
 
+
 	if event.type == pygame.KEYDOWN:
 		x_delta=0;
 		y_delta=0;
@@ -86,23 +143,50 @@ while not gameExit:
 			x_delta -= 10
 		if event.key == pygame.K_RIGHT:
 			x_delta += 10
-		if event.key == pygame.K_UP:
-			y_delta -= 10
-		if event.key == pygame.K_DOWN:
-			y_delta += 10
+		# if event.key == pygame.K_UP:
+		# 	y_delta -= 10
+		# if event.key == pygame.K_DOWN:
+		# 	y_delta += 10
 		# if event.key == pygame.K_LCTRL:
+
 
 	
 	x_pos +=x_delta
 	y_pos +=y_delta
-	gameDisplay.blit(meatballs1,(x_pos,y_pos))
+	if x_pos>=WIDTH:
+		poop_x = random.randrange(0, WIDTH) 
+		poop_y = random.randrange(200, HEIGHT) 
+		x_pos = 0
+		if bg_x == 0:
+			bg_x -=200
+		else:
+			bg_x = 0
+	pygame.display.update()	
+
+	for i in range(0,len(meatballSprites) -1):
+		# meatballArray[i].update()
+		# meatballArray[i].draw(gameDisplay)
+		meatballSprites[i].update()
+		meatballSprites[i].draw(gameDisplay)
+		display.update()
+	for i in range(0,len(spaghettiSprites) -1):
+		# meatballArray[i].update()
+		# meatballArray[i].draw(gameDisplay)
+		spaghettiSprites[i].update()
+		spaghettiSprites[i].draw(gameDisplay)
+		display.update()
+	# gameDisplay.blit(meatballs1,(x_pos,y_pos))
 	# gameDisplay.fill(black)
 	# pygame.display.update()		
 	# clock.tick(30)
 
-	sprites.update()
-	sprites.draw(gameDisplay)
-	display.update()
+	# sprites.update()
+	# sprites.draw(gameDisplay)
+	# display.update()
+	# # block.update()
+	# # block.draw(gameDisplay)
+	# meatballs1.draw(gameDisplay)
+	# meatballs1.update()
 #render is for words
 # class meatballs(pygame.sprite.Sprite):
 # 	def _init_(self, x, y)
@@ -118,15 +202,7 @@ while not gameExit:
 # 		hits += 1
 
 
-	if x_pos>=WIDTH:
-		poop_x = random.randrange(0, WIDTH) 
-		poop_y = random.randrange(200, HEIGHT) 
-		x_pos = 0
-		if bg_x == 0:
-			bg_x -=200
-		else:
-			bg_x = 0
-	pygame.display.update()		
+		
 
 #required
 pygame.quit()
