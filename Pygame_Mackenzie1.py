10ae284ad6855e667e1c933446f666ff134ce8f5
Dr Brown <Dr Brown>
2016-11-30 17:57:47 -0500

    sixteenth commit

--- Pygame_Mackenzie1.py
+++ Pygame_Mackenzie1.py
@@ -5,12 +5,10 @@ import pygame
 import random
 
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 544
 
-paused = False
+#800
 muted = False
-
-screen_height = 800
 
 white = (255,255,255)
 black = (0,0,0)
@@ -34,15 +32,23 @@ speed = 10
 
 score = 0
 maxscore = 100
-#create a surface
 
-#pygame.display.flip() 		#similar to a flip book, updates entire surface
-# pygame.display.update()		#only updates portion specified
-# this is the play sound function
 def play(soundFile):
 	soundFile = pygame.mixer.Sound(file=soundFile)
 	if not muted:
 		soundFile.play(loops=0)
+
+def overlap(Sprite):
+	for x in meatballArray:
+		if x != Sprite and Sprite.rect.colliderect(x.rect):
+			Sprite.rect.x = random.randint(1,WIDTH -20)
+			Sprite.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			overlap(Sprite)
+	for x in spaghettiArray:
+		if x != Sprite and Sprite.rect.colliderect(x.rect):
+			Sprite.rect.x = random.randint(1,WIDTH -20)
+			Sprite.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			overlap(Sprite)
 
 class Meatball(Sprite):
 	def __init__(self):
@@ -55,21 +61,21 @@ class Meatball(Sprite):
 		global meatballScore
 		if action == "move down":
 			self.rect.y += meatballspeed
-			if self.rect.y > screen_height:
+			if self.rect.y > HEIGHT- self.rect.height:
 				self.rect.x = random.randint(1,WIDTH -20)
 				self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+				overlap(self)
+				#y value is greater than the height it resets
 		elif action == "gotToTop":
 			self.rect.x = random.randint(1,WIDTH -20)
 			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			overlap(self)
 		if self.rect.colliderect(bowl1):
 			self.rect.x = random.randint(1,WIDTH -20)
 			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
 			score = score + meatballScore
-
-
-	def gotToTop(self):
-		self.rect.x = random.randint(1,WIDTH -20)
-		self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			#changes the score and goes back to top
+			overlap(self)
 
 class Spaghetti(Sprite):
 	def __init__(self):
@@ -83,32 +89,30 @@ class Spaghetti(Sprite):
 		global spaghettiScore
 		if action == "move down":
 			self.rect.y += spaghettispeed
-			if self.rect.y > screen_height:
+			if self.rect.y > HEIGHT - self.rect.height:
 				self.rect.x = random.randint(1,WIDTH -20)
 				self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+				overlap(self)
 		elif action == "gotToTop":
 			self.rect.x = random.randint(1,WIDTH -20)
 			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			overlap(self)
 		if self.rect.colliderect(bowl1):
 			self.rect.x = random.randint(1,WIDTH -20)
 			self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
 			score = score + spaghettiScore
-			# this is for playing sound
-			# play(file name here ex: 'getPoint.wav')
-	def gotToTop(self):
-		self.rect.x = random.randint(1,WIDTH -20)
-		self.rect.y = (random.randint(0, HEIGHT-30))*(-1)
+			overlap(self)
 
 class Bowl(Sprite):
 	def __init__(self):
 		Sprite.__init__(self)
 		self.image = image.load("bowl_v2.bmp").convert_alpha()
 		self.rect = self.image.get_rect()
-		self.rect.y = 450
+		self.rect.y = HEIGHT - self.rect.height
 
 	def update(self):
 		self.rect.x = x_pos
-		self.rect.y = 450
+		self.rect.y = HEIGHT - self.rect.height
 		if self.rect.x < 0:
 			self.rect.x = 0
 		elif self.rect.x > (WIDTH - self.rect.width):
@@ -116,30 +120,31 @@ class Bowl(Sprite):
 
 class Background(Sprite):
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        Sprite.__init__(self)
         self.image = pygame.image.load("background2.bmp")
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
 
 init()
+#initiates the pygame lib
 
 gameDisplay = display.set_mode((WIDTH, HEIGHT)) #initialize with a tuple
 #lets add a title, aka "caption"
 display.set_caption("Cloudy with a Chance of Meatballs")
 
-scoreFont = font.Font(None, 25)
+scoreFont = font.Font(None, 30)
 
 BackGround = Background("background2.bmp", [0,0])
 
-# meatball = Meatball()
-# meatballSprite = RenderPlain(meatball)
 meatballArray = []
 for i in range(0, maxmeatballs-1):
 	meatballArray.append(Meatball())
 meatballSprites = []
 for x in  range(0, (len(meatballArray)-1)):
+	#zero to the length of the array
 	meatballSprites.append(RenderPlain(meatballArray[x]))
+	#add the rendered meatball sprite x- adding it to meatball sprites
 spaghettiArray = []
 for i in range(0, maxspaghettis-1):
 	spaghettiArray.append(Spaghetti())
@@ -154,40 +159,18 @@ gameDisplay.fill(white)
 gameExit = False
 
 while not gameExit:
-#if it collides then make the sound 
-	# if score == maxscore:
-	# 	gameExit = True
-	# This sometimes works, and is used to stop the game when you get a max score
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
 	if event.type == pygame.KEYDOWN:
-		y_pos= 450;
 		if event.key == pygame.K_LEFT or event.key == pygame.K_a: # left key or a key pressed
-			# if you want to be able to pause uncomment the if statement with the other code in it
-			# if not paused:
 			x_pos -= speed
-			# bowlthing.move(-10)
 		if event.key == pygame.K_RIGHT or event.key == pygame.K_d: # right key or d key pressed
-			# if you want to be able to pause uncomment the if statement with the other code in it
-			# if not paused:
 			x_pos += speed
-		# uncomment this code if you want muting. key m toggles mute
-		# if event.key == pygame.K_m:
-		# 	muted = not muted
-		# un comment the code below this comment to allow pauses. space or p key toggles mute
-		# if event.key == pygame.K_SPACE or event.key == pygame.K_p:
-		# 	paused = not paused
-	# suround all code in this if statement in the while loop under this comment if you want to be able to pause the game but keep display.update() out of the if statement
-	# if not paused:
 	gameDisplay.fill(white)
 	gameDisplay.blit(BackGround.image, BackGround.rect)
 	scoreText = scoreFont.render("Score: "+str(score), False, (0,0,0))
 	gameDisplay.blit(scoreText, (320, 0))
-	# pygame.display.update()
-	# meatballSprite.update('move down')
-	# meatballSprite.draw(gameDisplay)
 	for i in range(0,len(meatballSprites) -1):
 		meatballSprites[i].update("move down")
 		meatballSprites[i].draw(gameDisplay)
@@ -199,5 +182,5 @@ while not gameExit:
 	display.update()
 
 pygame.quit()
-quit()				#exits python
+quit()			
 
